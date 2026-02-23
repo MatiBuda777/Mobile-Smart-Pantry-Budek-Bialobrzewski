156a00c38321284b2532141ac61aed2c8f9f85c7
mateuszbudek <mateusz.budek@cyfroweszkoly.pl>
2026-02-23 14:29:24 +0100

    start work on reading JSON

--- app/src/main/java/com/example/mobile_smart_pantry_project_iv/MainActivity.kt
+++ app/src/main/java/com/example/mobile_smart_pantry_project_iv/MainActivity.kt
@@ -1,14 +1,20 @@
 package com.example.mobile_smart_pantry_project_iv
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.mobile_smart_pantry_project_iv.databinding.ActivityMainBinding
+import com.example.mobile_smart_pantry_project_iv.model.Product
+import kotlinx.serialization.json.Json
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
+    lateinit var products: List<Product>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +27,22 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+    }
+
+    private fun readFromJson(){
+        try{
+            val file = File(filesDir, "pantry.json")
+            if (!file.exists()) return
+
+            val jsonString = file.readText()
+            val json = Json { ignoreUnknownKeys = true }
+            val loadedList = json.decodeFromString<List<Product>>(jsonString) // finish this function
+
+            Toast.makeText(this, "Loaded products from JSON", Toast.LENGTH_SHORT).show()
+        } catch (ex: Exception){
+            Toast.makeText(this, "Exception appeared", Toast.LENGTH_SHORT).show()
+            Log.e("readFromJson exception", "$ex")
         }
     }
 }
